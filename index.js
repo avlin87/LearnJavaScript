08f91d5d790984ec785ae038615bc62eb0df487b
Aleksandr Liadov <aleksandr.liadov@etc-consult.de>
2024-03-24 02:40:14 -0600

    function expression

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const movies = [
-    { title: 'a', year: 2018, raiting: 4.5 },
-    { title: 'b', year: 2018, raiting: 4.7 },
-    { title: 'c', year: 2018, raiting: 3 },
-    { title: 'd', year: 2017, raiting: 4.5 }
-];
+walk();
 
-const output = movies
-     .sort((a, b) => b.raiting - a.raiting)
-     .filter(movie => movie.raiting >= 4 && movie.year === 2018)
-     .map(movie => movie.title);
+function walk() {
+    console.log('walk');
+}
 
-console.log(output);
+console.log(x);
+let x = 1;
+
+const run = function() {
+    console.log('run');
+};
+
+const fast = run;
+
+fast();
